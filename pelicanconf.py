c3583c789ebbf1ecc82b708001b04c9851090560
Adrian Martinez <n.bicalcarata@gmail.com>
2020-04-12 17:07:31 -0500

    Update settings from quickstart

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,27 +1,29 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = u'Adri\xe1n Mart\xednez'
-SITENAME = u'Ziggurat Vertigo'
-SITESUBTITLE = u'Apuntes diversos y hojas trampa'
-
-PATH = 'content'
-# THEME = "/home/adrian/pelican-themes/nikhil-theme"
-# THEME = "/home/adrian/pelican-themes/storm"
+AUTHOR = 'Adrián Martínez'
+SITENAME = 'Adrián Martínez'
+SITESUBTITLE = 'Mis apuntes y hojas trampa'
 THEME = "pujangga"
-
-TIMEZONE = 'America/Monterrey'
 
 DATE_FORMATS = {
     'en': '%a, %d %b %Y',
     'es': '%A, %d de %B del %Y',
     'jp': '%Y-%m-%d(%a)',
 }
+
+GITHUB_URL = 'http://github.com/nbicalcarata/'
+
 SHOW_CONTENT_SUMMARY_ON_INDEX = True
-DEFAULT_LANG = u'es'
-LOCALE='es_MX.utf8'
+SUMMARY_MAX_LENGTH = 0
+
+LOCALE = 'es_MX.utf8'
 
 DEFAULT_DATE_FORMAT = {'%A %d %B %Y'}
+PATH = 'content'
+TIMEZONE = 'America/Monterrey'
+DEFAULT_LANG = 'es'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -29,39 +31,19 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
+DISPLAY_PAGES_ON_MENU = True
 
 # Blogroll
 # LINKS = (('Pelican', 'http://getpelican.com/'),
-         # ('Python.org', 'http://python.org/'),
-         # ('Jinja2', 'http://jinja.pocoo.org/'),
-         # ('You can modify those links in your config file', '#'),)
+#          ('Python.org', 'http://python.org/'),
+#          ('Jinja2', 'http://jinja.pocoo.org/'),
+#          ('You can modify those links in your config file', '#'),)
 
 # Social widget
 # SOCIAL = (('You can add links in your config file', '#'),
-          # ('Another social link', '#'),)
+#           ('Another social link', '#'),)
 
-DEFAULT_PAGINATION = 4
-
-# DEFAULT_PAGINATION = False
-# SUMMARY_MAX_LENGTH = 30
-# DISPLAY_CATEGORIES_ON_MENU = False
-DISPLAY_PAGES_ON_MENU = True
-
-# MENUITEMS = (
-    # ('Inicio', ''),
-    # ('Acerca de',  '/pages/acerca-de.html'),
-    # ('CV', '')
-    # )
+DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-RELATIVE_URLS = False
-
-STATIC_PATHS = ['images' ]
-
-# PYGMENTS_RST_OPTIONS = {'linenos': 'table'}
-
-PLUGINS = [
-    # ...
-    'pelican_gist',
-    # ...
-]
+# RELATIVE_URLS = True
